078cb60d568f52e62d7fe070d12d3d7e5e02fb1f
timothée bénédet (Ezzmazz) <timothee.benedet@gmail.com>
2025-05-20 16:49:14 +0000

    Change le bouton "Nouvelle recette" pour "New recipe"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -96,7 +96,7 @@ export default function HomePage() {
          <Button asChild variant="outline">
           <Link href="/recipes/new">
             <PlusCircle className="mr-2 h-4 w-4" />
-            Nouvelle recette
+            New recipe
           </Link>
         </Button>
       </div>
